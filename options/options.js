376c8bb3ee76518e6ff6661d973636558e68527d
Crowdbotics <engineering@crowdbotics.com>
2023-11-06 18:13:58 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "floral-leaf-dev-127314",
+  url: "https://floral-leaf-dev-127314.botics.co",
+  api: "https://floral-leaf-dev-127314.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
